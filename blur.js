23632416b63d55ae1331dd562c48cd511d0ded4e
motttey <motitago@gmail.com>
2021-11-09 21:04:20 +0900

    draw blur of dora

--- blur.js
+++ blur.js
@@ -1,5 +1,7 @@
-const cyan_size = 100;
-const white_size = 120;
+const blue_size = 120;
+const white_size = 250;
+const yellow_circle_size = 50;
+const red_circle_size = 75;
 
 let sketch = function(p) {
   p.setup = function(){
@@ -13,12 +15,21 @@ let sketch = function(p) {
     //着色
     p.strokeWeight(0);
 
-    p.fill(100,255,255);
-    p.square(p.random(p.width),p.random(p.height),p.random(cyan_size));
-
     p.fill(255,255,255);
     p.square(p.random(p.width),p.random(p.height),p.random(white_size));
     p.circle(p.random(p.width),p.random(p.height),p.random(white_size));
+
+    p.fill(255,255,0);
+    p.circle(p.random(p.width),p.random(p.height),p.random(yellow_circle_size));
+
+    // p.strokeWeight(1);
+    p.fill(255,25,50);
+    p.circle(p.random(p.width),p.random(p.height),p.random(red_circle_size));
+
+    p.fill(0,188,214);
+    p.square(p.random(p.width),p.random(p.height),p.random(blue_size));
+    p.square(p.random(p.width),p.random(p.height),p.random(blue_size));
+    p.circle(p.random(p.width),p.random(p.height),p.random(blue_size));
 
     p.filter(p.BLUR, 5)
   }
