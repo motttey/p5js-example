95146e3d87dd22d3e0437e2695d7166ebfda69fb
motttey <motitago@gmail.com>
2022-05-08 20:36:10 +0900

    fix: add color shift on img

--- glitch.js
+++ glitch.js
@@ -1,16 +1,37 @@
 const width = 1000;
-const height = 600;
-
-let libs = ["includes/libs/p5.glitch.js"];
-let glitch = new Glitch();
+const height = 1000;
+let image;
 
 let sketch = function(p) {
+  function draw_color_glitch(img, shift_size){
+    p.background(0);
+
+    let left_color = p.color(255, 0, 0);
+    let right_color = p.color(0, 255, 255);
+
+    p.push();
+    p.blendMode(p.ADD);
+
+    p.tint(left_color);
+    p.image(img, -shift_size, 0);
+
+    p.tint(right_color);
+    p.image(img, shift_size, 0);
+    p.pop();
+
+    let img_glitch = p.get();
+    p.clear();
+
+    return img_glitch;
+  }
+
   p.setup = function(){
     // createCanvas(600,425);
-    p.createCanvas(1000, 600);
+    p.createCanvas(1000, 1000);
     p.background(255,255,255);
 
     p.loadImage('rakuimon2.jpg', function(img){
+      /*
       glitch.loadType('jpg');
       glitch.loadQuality(.25);
       glitch.loadImage('./rakuimon2.jpg');
@@ -22,7 +43,8 @@ let sketch = function(p) {
       glitch.replaceHex('ffdb00430001','ffdb004300ff');
 
       glitch.buildImage();
-      p.image(glitch.image, 0, 0);
+      */
+      image = img;
     });
   }
 
@@ -31,6 +53,8 @@ let sketch = function(p) {
     //着色
     p.strokeWeight(0);
     // p.fill(255,255,255);
+    p.image(draw_color_glitch(image, p.random(5)), 0, 0);
+
   }
 }
 new p5(sketch, 'container');
