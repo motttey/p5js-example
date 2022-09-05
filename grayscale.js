ac78026858e6ee802319f8d5c8573a5c07f1fc01
motttey <motitago@gmail.com>
2022-09-05 21:39:06 +0900

    fix: randomly setting triangle

--- grayscale.js
+++ grayscale.js
@@ -1,5 +1,7 @@
 let sketch = function(p) {
     let img1, img2;
+    let circleMask;
+
     p.setup = function(){
         p.loadImage('./internetdora.jpg', function(ret){
             img1 = ret;
@@ -9,16 +11,24 @@ let sketch = function(p) {
             p.filter(p.GRAY);
 
             circleMask = p.createGraphics(128, 128);
-            circleMask.triangle(0, 75, 68, 20, 120, 108);
+
+            // (x1, y1),  (x2, y2),  (x3, y3)
+            circleMask.triangle(
+                p.random(0, 30),
+                p.random(60, 75),
+                p.random(50, 80),
+                p.random(0, 30),
+                p.random(100, 130),
+                p.random(90, 120)
+            );
             img1.mask(circleMask);
 
             p.image(img1, 0, 0);
         });
         
-        // createCanvas(600,425);
         p.createCanvas(1000, 1000);
         p.background(255,255,255);
-    }
-}
+    };
+};
 new p5(sketch, 'container');
   
